6eb2817c292f424e34d9893c4c0cc7d9f5b78897
Kai <Haunv1503@gmail.com>
2023-11-16 17:49:09 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,86 @@
 <template>
   <div>
     <section class="w-full h-[500px]">
-      <Swiper class="h-full" :modules="[SwiperAutoplay, SwiperEffectCreative]" :slides-per-view="1" :loop="true"
-        :effect="'creative'" :autoplay="{
+      <Swiper
+        class="h-full"
+        :modules="[SwiperAutoplay, SwiperEffectCreative]"
+        :slides-per-view="1"
+        :loop="true"
+        :effect="'creative'"
+        :autoplay="{
           delay: 8000,
           disableOnInteraction: true,
-        }" :creative-effect="{
-  prev: {
-    shadow: false,
-    translate: ['-20%', 0, -1],
-  },
-  next: {
-    translate: ['100%', 0, 0],
-  },
-}">
+        }"
+        :creative-effect="{
+          prev: {
+            shadow: false,
+            translate: ['-20%', 0, -1],
+          },
+          next: {
+            translate: ['100%', 0, 0],
+          },
+        }"
+      >
         <SwiperSlide>
-          <img src="https://cf.shopee.vn/file/vn-50009109-c01ffe7818db36dd90e0c8ed0ec2baa1_xxhdpi" />
+          <img
+            src="https://cf.shopee.vn/file/vn-50009109-c01ffe7818db36dd90e0c8ed0ec2baa1_xxhdpi"
+          />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src="https://cf.shopee.vn/file/vn-50009109-ed9104cb971e205afcdd0bf1cf5479c1_xxhdpi" />
+          <img
+            src="https://cf.shopee.vn/file/vn-50009109-ed9104cb971e205afcdd0bf1cf5479c1_xxhdpi"
+          />
         </SwiperSlide>
 
         <SwiperSlide>
-          <img src="https://cf.shopee.vn/file/vn-50009109-83dd5db23e7386abacc6be4165b2cf2e_xxhdpi" />
+          <img
+            src="https://cf.shopee.vn/file/vn-50009109-83dd5db23e7386abacc6be4165b2cf2e_xxhdpi"
+          />
         </SwiperSlide>
       </Swiper>
     </section>
 
     <section class="mt-[30px] w-full">
       <div class="flex gap-[20px] justify-center mb-[15px] tabar_product">
-        <h4 @click="onChangeActiveProductTab(1)"
-          :class="'text-[18px] pb-2 cursor-pointer hover:text-orange-500 duration-150 ease-in-out uppercase' + (currentActiveProductTab == 1 ? ' activated text-orange-500' : '')">
-          Sản phẩm bán chạy</h4>
-        <h4 @click="onChangeActiveProductTab(2)"
-          :class="'text-[18px] pb-2 cursor-pointer hover:text-orange-500 duration-150 ease-in-out uppercase' + (currentActiveProductTab == 2 ? ' activated text-orange-500' : '')">
-          Sản phẩm mới cập nhật</h4>
+        <h4
+          @click="onChangeActiveProductTab(1)"
+          :class="
+            'text-[18px] pb-2 cursor-pointer hover:text-orange-500 duration-150 ease-in-out uppercase' +
+            (currentActiveProductTab == 1 ? ' activated text-orange-500' : '')
+          "
+        >
+          Sản phẩm bán chạy
+        </h4>
+        <h4
+          @click="onChangeActiveProductTab(2)"
+          :class="
+            'text-[18px] pb-2 cursor-pointer hover:text-orange-500 duration-150 ease-in-out uppercase' +
+            (currentActiveProductTab == 2 ? ' activated text-orange-500' : '')
+          "
+        >
+          Sản phẩm mới cập nhật
+        </h4>
       </div>
-
-
 
       <a-spin :spinning="isLoadingProduct">
         <div class="flex flex-wrap gap-[20px] px-[30px] w-full justify-center">
-          <div class="p-[10px]" style="width: 23%; border: none; box-shadow: none;" :key="index"
-            v-for="(item, index) in productList">
+          <div
+            class="p-[10px]"
+            style="width: 23%; border: none; box-shadow: none"
+            :key="index"
+            v-for="(item, index) in productList"
+          >
             <div class="relative">
-              <router-link :to="`/san-pham/${item?.tieuDe}/${item?.id}`">
-                <img :src="item?.anhChinh?.url" class="shadow-sm h-[280px] rounded-[5px]" />
+              <router-link class="block h-[450px]" :to="`/san-pham/${item?.tieuDe}/${item?.id}`">
+                <img
+                  :src="item?.anhChinh?.url"
+                  class="shadow-sm h-full rounded-[5px]"
+                />
               </router-link>
 
               <div class="absolute bottom-2 left-[30%] hidden">
-                <button>
-                  Xem chi tiết
-                </button>
+                <button>Xem chi tiết</button>
               </div>
             </div>
 
@@ -63,10 +91,8 @@
                 </router-link>
               </h3>
 
-
               <div class="product_price flex items-center gap-[10px]">
                 <template v-if="item?.giaCu && item?.giaCu > 0">
-
                   <del>{{ _formatVnCurrency(item?.giaCu) }}</del>
                   <span class="font-bold text-red-500">
                     {{ _formatVnCurrency(item?.giaMoi) }}
@@ -74,59 +100,63 @@
                 </template>
 
                 <span v-else class="font-bold text-red-500">
-                    {{ _formatVnCurrency(item?.giaMoi) }}
-                  </span>
+                  {{ _formatVnCurrency(item?.giaMoi) }}
+                </span>
               </div>
 
               <a-space>
-                <a-rate class="text-[14px]" :value="item?.soSaoDanhGia || 0" allow-half disabled />
+                <a-rate
+                  class="text-[14px]"
+                  :value="item?.tbDanhGia || 0"
+                  allow-half
+                  disabled
+                />
                 <a-divider type="vertical" class="bg-gray-500" />
-                <span>55 Đã bán</span>
+                <span>{{item?.daBan}} Đã bán</span>
               </a-space>
             </a-space>
           </div>
         </div>
       </a-spin>
 
-      <div class="flex justify-center mt-[20px]">
+      <div class="flex justify-center my-[20px]">
         <a-button class="bg-black text-white">
           <router-link to="/dat-hang"> Xem thêm </router-link>
         </a-button>
       </div>
     </section>
-
   </div>
 </template>
 
 <script lang="ts" setup>
-import ProductService from '~/services/ProductService';
-
+import ProductService from "~/services/ProductService";
 
 const _formatVnCurrency = inject("formatVnCurrency", (p: number) => 0);
 
 const currentActiveProductTab = ref<number>(1);
 const isLoadingProduct = ref<boolean>(false);
-const onChangeActiveProductTab = (val: number) => currentActiveProductTab.value = val;
+const onChangeActiveProductTab = (val: number) =>
+  (currentActiveProductTab.value = val);
 const productList = ref([]);
-
 
 onMounted(() => {
   if (isLoadingProduct.value) return;
   isLoadingProduct.value = true;
   ProductService.locSp({
-    page: 0, size: 8
+    page: 0,
+    size: 8,
   })
     .then((res: any) => {
       console.log("filter product: ", res.content);
       productList.value = res.content;
     })
-    .catch(err => {
+    .catch((err) => {
       notification.error({
-        message: "Không thể lấy dữ liệu sản phẩm!"
+        message: "Không thể lấy dữ liệu sản phẩm!",
       });
       console.log("filter product failed: ", err);
     })
-    .finally(() => isLoadingProduct.value = false);
+    .finally(() => (isLoadingProduct.value = false));
 });
 </script>
 
--- pages/san-pham/[name]/[id].vue
+++ pages/san-pham/[name]/[id].vue
@@ -1,6 +1,6 @@
 <template>
   <a-spin :spinning="isGettingProduct" size="large">
-    <div>
+    <div class="max-w-[1400px] mx-auto">
       <a-breadcrumb class="bg-gray-50 px-3 py-2 md:px-[50px]">
         <a-breadcrumb-item>
           <router-link to="/">Trang chủ</router-link>
@@ -12,24 +12,45 @@
       <div class="px-3 md:px-[50px]">
         <!-- begin product info -->
         <div class="md:flex gap-[50px]">
-          <a-space class="product_image md:w-1/2 max-w-[350px] md:max-w-[unset] mx-auto">
-            <swiper :direction="'vertical'" :slidesPerView="8" :pagination="{
-              clickable: true,
-            }" :autoplay="{
-  delay: 2000,
-  disableOnInteraction: true,
-}" :loop="true" :modules="modules" class="product_image_slide mx-auto h-[450px] w-[50px]" @swiper="onSwiper"
-              @slideChange="onSlideChange">
-              <swiper-slide v-slot="{ isActive }" v-for="item in productImages" :key="item" class="mt-[10px] h-[40px]">
-                <div :style="isActive
-                  ? 'border: 1px solid yellow'
-                  : 'border: 1px solid #d9d9d9'
-                  ">
+          <a-space
+            style="max-width: 600px; max-height: 500px;"
+            class="product_image md:w-1/2 justify-center md:max-w-[unset] mx-auto"
+          >
+            <swiper
+              :direction="'vertical'"
+              :slidesPerView="8"
+              :pagination="{
+                clickable: true,
+              }"
+              :autoplay="{
+                delay: 2000,
+                disableOnInteraction: true,
+              }"
+              :loop="true"
+              :modules="modules"
+              class="product_image_slide mx-auto h-[450px] w-[50px]"
+              @swiper="onSwiper"
+              @slideChange="onSlideChange"
+            >
+              <swiper-slide
+                v-slot="{ isActive }"
+                v-for="item in productImages"
+                :key="item"
+                class="mt-[10px] h-[40px]"
+              >
+                <div
+                  @click="onClickImageBg(item)"
+                  :style="
+                    isActive
+                      ? 'border: 1px solid yellow'
+                      : 'border: 1px solid #d9d9d9'
+                  "
+                >
                   <img class="" alt="example" :src="item" />
                 </div>
               </swiper-slide>
             </swiper>
-            <img :src="activeImage" />
+            <img class="w-full max-h-[500px] mx-auto" :src="activeImage" />
           </a-space>
 
           <div class="product_info md:w-1/2" v-if="productDetail">
@@ -41,26 +62,38 @@
               <div class="mr-3 inline-block">
                 Mã SP: {{ productDetail?.maSP }}
               </div>
-              <a-rate v-model:value="productRate" allow-half disabled /> (55 lượt
-              đánh giá)
+              <a-rate
+                :value="productDetail?.tbDanhGia || 0"
+                allow-half
+                disabled
+              />
               <a-divider type="vertical" class="bg-gray-500" />
-              <span>55 Đánh giá</span>
+              <span>{{ productDetail?.soDanhGia || 0 }} Đánh giá</span>
               <a-divider type="vertical" class="bg-gray-500" />
-              <span>55 Đã bán</span>
+              <span>{{ productDetail?.daBan || 0 }} Đã bán</span>
             </div>
 
-            <div class="product_price flex items-center gap-[20px] bg-gray-100 px-5 py-3 mt-3">
-              <span class="font-bold text-xl text-red-500">
+            <div
+              class="product_price flex items-center gap-[20px] bg-gray-100 px-5 py-3 mt-3"
+            >
+              <template v-if="productDetail?.giaCu && productDetail?.giaCu > 0">
+                <span class="font-bold text-xl text-red-500">
+                  {{ _formatVnCurrency(productDetail?.giaMoi || 0) }}
+                </span>
+                <div class="flex items-center gap-[10px] text-[14px]">
+                  <del>{{ _formatVnCurrency(productDetail?.giaCu || 0) }}</del>
+                  <span class="bg-red-200 text-red-500 p-1"
+                    >-{{
+                      String(
+                        (productDetail?.giaMoi / productDetail?.giaCu) * 100
+                      ).slice(0, 2)
+                    }}%</span
+                  >
+                </div>
+              </template>
+              <span v-else class="font-bold text-xl">
                 {{ _formatVnCurrency(productDetail?.giaMoi || 0) }}
               </span>
-              <div class="flex items-center gap-[10px] text-[14px]">
-                <del>{{ _formatVnCurrency(productDetail?.giaCu || 0) }}</del>
-                <span class="bg-red-200 text-red-500 p-1">-{{
-                  String(
-                    (productDetail?.giaMoi / productDetail?.giaCu) * 100
-                  ).slice(0, 2)
-                }}%</span>
-              </div>
             </div>
 
             <fieldset class="vourcher border-dashed mt-3 p-0">
@@ -68,7 +101,10 @@
                 Chương trình khuyến mãi
               </legend>
               <ul>
-                <template :key="index" v-for="(item, index) in productDetail.vouchers">
+                <template
+                  :key="index"
+                  v-for="(item, index) in productDetail.vouchers"
+                >
                   <li>
                     {{ item.moTa }}
                   </li>
@@ -80,20 +116,40 @@
 
             <a-space direction="vertical" :size="20">
               <a-space class="product_variations mt-5">
-                <a-select v-if="productDetail?.loaiBienThe == 'BOTH' ||
-                  productDetail?.loaiBienThe == 'COLOR'
-                  " v-model:value="selectedVariation1" @change="onThayDoiBienTheMau" class="min-w-[200px]">
+                <a-select
+                  v-if="
+                    productDetail?.loaiBienThe == 'BOTH' ||
+                    productDetail?.loaiBienThe == 'COLOR'
+                  "
+                  v-model:value="selectedVariation1"
+                  @change="onThayDoiBienTheMau"
+                  class="min-w-[200px]"
+                >
                   <a-select-option value="">Chọn màu</a-select-option>
-                  <a-select-option :key="index" :value="item.id" v-for="(item, index) in variation1List">
+                  <a-select-option
+                    :key="index"
+                    :value="item.id"
+                    v-for="(item, index) in variation1List"
+                  >
                     {{ item.giaTri }}
                   </a-select-option>
                 </a-select>
 
-                <a-select v-if="productDetail?.loaiBienThe == 'BOTH' ||
-                  productDetail?.loaiBienThe == 'SIZE'
-                  " v-model:value="selectedVariation2" @change="onThayDoiBienTheSize" class="min-w-[200px]">
+                <a-select
+                  v-if="
+                    productDetail?.loaiBienThe == 'BOTH' ||
+                    productDetail?.loaiBienThe == 'SIZE'
+                  "
+                  v-model:value="selectedVariation2"
+                  @change="onThayDoiBienTheSize"
+                  class="min-w-[200px]"
+                >
                   <a-select-option value="">Chọn size</a-select-option>
-                  <a-select-option :key="index" :value="item.id" v-for="(item, index) in variation2List">
+                  <a-select-option
+                    :key="index"
+                    :value="item.id"
+                    v-for="(item, index) in variation2List"
+                  >
                     {{ item.giaTri }}
                   </a-select-option>
                 </a-select>
@@ -101,11 +157,24 @@
 
               <a-space :size="20">
                 <span> Số lượng: </span>
-                <a-input-number v-model:value="quantity" min="1" :max="variationStockCnt" />
-                <span v-if="variationStockCnt > 0">{{ variationStockCnt }} sản phẩm có sẵn</span>
+                <a-input-number
+                  v-model:value="quantity"
+                  min="1"
+                  :max="variationStockCnt"
+                />
+                <span v-if="variationStockCnt > 0"
+                  >{{ variationStockCnt }} sản phẩm có sẵn</span
+                >
                 <span v-else>sản phẩm đang hết hàng</span>
               </a-space>
-              <a-button :disabled="variationStockCnt == 0" @click="addToCart">Thêm giỏ hàng</a-button>
+              <a-space>
+                <a-button :disabled="variationStockCnt == 0" @click="addToCart"
+                  >Thêm giỏ hàng</a-button
+                >
+                <a-button :disabled="variationStockCnt == 0" @click="buyNow">
+                  Mua ngay
+                </a-button>
+              </a-space>
             </a-space>
           </div>
         </div>
@@ -120,11 +189,21 @@
           <!-- product info additional -->
           <a-tab-pane key="2" tab="Thông tin thêm" force-render>
             <a-descriptions bordered :column="1" class="max-w-[800px]">
-              <a-descriptions-item label="Chất liệu">{{ productDetail.chatLieu }}</a-descriptions-item>
-              <a-descriptions-item label="Trọng lượng">{{ productDetail.trongLuong }}g</a-descriptions-item>
-              <a-descriptions-item label="Công nghệ">{{ productDetail.congNghe }}</a-descriptions-item>
-              <a-descriptions-item label="Tính năng nổi bật">{{ productDetail.tinhNang }}</a-descriptions-item>
-              <a-descriptions-item label="Nơi sản xuất">{{ productDetail.noiSanXuat }}</a-descriptions-item>
+              <a-descriptions-item label="Chất liệu">{{
+                productDetail.chatLieu
+              }}</a-descriptions-item>
+              <a-descriptions-item label="Trọng lượng"
+                >{{ productDetail.trongLuong }}g</a-descriptions-item
+              >
+              <a-descriptions-item label="Công nghệ">{{
+                productDetail.congNghe
+              }}</a-descriptions-item>
+              <a-descriptions-item label="Tính năng nổi bật">{{
+                productDetail.tinhNang
+              }}</a-descriptions-item>
+              <a-descriptions-item label="Nơi sản xuất">{{
+                productDetail.noiSanXuat
+              }}</a-descriptions-item>
             </a-descriptions>
           </a-tab-pane>
 
@@ -133,14 +212,18 @@
             <a-comment>
               <template #author><a>Han Solo</a></template>
               <template #avatar>
-                <a-avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />
+                <a-avatar
+                  src="https://joeschmoe.io/api/v1/random"
+                  alt="Han Solo"
+                />
               </template>
               <template #content>
                 <a-rate v-model:value="productRate" allow-half disabled />
                 <p class="m-0">
-                  We supply a series of design principles, practical patterns and
-                  high quality design resources (Sketch and Axure), to help people
-                  create their product prototypes beautifully and efficiently.
+                  We supply a series of design principles, practical patterns
+                  and high quality design resources (Sketch and Axure), to help
+                  people create their product prototypes beautifully and
+                  efficiently.
                 </p>
               </template>
               <template #datetime>
@@ -155,12 +238,25 @@
         <!-- end product description -->
 
         <section class="mt-[30px]">
-          <h3 class="m-0 w-fit" style="border-bottom: 3px solid rgba(220, 220, 220, 0.747); font-size: 18px">
+          <h3
+            class="mb-[20px] w-fit"
+            style="
+              border-bottom: 3px solid rgba(220, 220, 220, 0.747);
+              font-size: 18px;
+            "
+          >
             Sản phẩm liên quan
           </h3>
-          <div class="flex flex-wrap gap-[20px] px-[30px] w-full justify-center">
-            <a-card hoverable :bodyStyle="'padding: 10px'" style="width: 23%; border: none; box-shadow: none;"
-              :key="index" v-for="(item, index) in relatedProducts">
+          <div
+            class="flex flex-wrap gap-[20px] px-[30px] w-full justify-center"
+          >
+            <a-card
+              hoverable
+              :bodyStyle="'padding: 10px'"
+              style="width: 23%; border: none; box-shadow: none"
+              :key="index"
+              v-for="(item, index) in relatedProducts"
+            >
               <template #cover>
                 <div class="relative">
                   <router-link :to="`/san-pham/${item?.tieuDe}/${item?.id}`">
@@ -168,12 +264,9 @@
                   </router-link>
 
                   <div class="absolute bottom-2 left-[30%] hidden">
-                    <button>
-                      Xem chi tiết
-                    </button>
+                    <button>Xem chi tiết</button>
                   </div>
                 </div>
-
               </template>
               <a-card-meta>
                 <template #title>
@@ -182,10 +275,8 @@
                       {{ item.tieuDe }}
                     </router-link>
                   </h3>
-
                 </template>
                 <template #description>
-
                   <div class="product_price flex items-center gap-[10px]">
                     <del>{{ item?.giaCu }} vnd</del>
                     <span class="font-bold text-red-500">
@@ -194,7 +285,12 @@
                   </div>
 
                   <a-space>
-                    <a-rate class="text-[14px]" :value="item?.soSaoDanhGia || 0" allow-half disabled />
+                    <a-rate
+                      class="text-[14px]"
+                      :value="item?.soSaoDanhGia || 0"
+                      allow-half
+                      disabled
+                    />
                     <a-divider type="vertical" class="bg-gray-500" />
                     <span>55 Đã bán</span>
                   </a-space>
@@ -204,7 +300,6 @@
           </div>
         </section>
       </div>
-
     </div>
   </a-spin>
 </template>
@@ -218,6 +313,7 @@ import ProductService from "~/services/ProductService";
 
 const _storeCart = useCartStore();
 const _route = useRoute();
+const _router = useRouter();
 const _formatVnCurrency = function (value: number) {
   return Intl.NumberFormat("vi-VN", {
     style: "currency",
@@ -239,12 +335,13 @@ const variation1List = ref([]);
 const variation2List = ref([]);
 
 const variationStockCnt = ref<number>(0);
+const currentProductVariationId = ref<number>();
 
 const quantity = ref<number>(1);
 
 const onThayDoiBienTheMau = (value: string) => {
   console.log("variation 1: ", value);
-
+  currentProductVariationId.value = undefined;
   quantity.value = 1;
   if (!value) {
     variation2List.value = productDetail.value.giaTri2List;
@@ -277,7 +374,7 @@ const onThayDoiBienTheMau = (value: string) => {
 };
 const onThayDoiBienTheSize = (value: string) => {
   console.log("variation 2: ", value);
-
+  currentProductVariationId.value = undefined;
   quantity.value = 1;
   if (!value) {
     variation1List.value = productDetail.value.giaTri1List;
@@ -318,8 +415,8 @@ const addToCart = (product, qty) => {
     return;
   }
   product.quantity = qty || 1;
-  
-  if(!productDetail.value) return;
+
+  if (!productDetail.value) return;
 
   if (productDetail.value.loaiBienThe == "BOTH") {
     console.log("add cart - both");
@@ -387,7 +484,7 @@ const addToCart = (product, qty) => {
       const productVariation = productDetail.value.bienTheDTOS.find(
         (item) => item.giatri2 == Number(selectedVariation2.value)
       );
-      console.log("variation: ", productVariation)
+      console.log("variation: ", productVariation);
       if (productVariation) {
         console.log("add variation: ", productVariation);
 
@@ -406,10 +503,73 @@ const addToCart = (product, qty) => {
   }
 };
 
+const buyNow = () => {
+  if (productDetail.value.loaiBienThe == "BOTH") {
+    console.log("add cart - both");
+    if (!selectedVariation1.value || !selectedVariation2.value) {
+      notification.warning({
+        message: "Vui lòng chọn phân loại sản phẩm!",
+      });
+      return;
+    }
+    const productVariation = productDetail.value.bienTheDTOS.find(
+      (item) =>
+        item.giatri1 == Number(selectedVariation1.value) &&
+        item.giatri2 == Number(selectedVariation2.value)
+    );
+
+    if (productVariation) {
+      _router.push("/dat-hang?type=NOW&id=" + productVariation.id);
+      console.log("buy now variation: ", productVariation);
+    }
+  } else {
+    if (productDetail.value.loaiBienThe == "COLOR") {
+      console.log("add cart - color");
+      // for color
+      if (!selectedVariation1) {
+        notification.warning({
+          message: "Vui lòng chọn phân loại sản phẩm!",
+        });
+        return;
+      }
+
+      const productVariation = productDetail.value.bienTheDTOS.find(
+        (item) => item.giatri1 == Number(selectedVariation1.value)
+      );
+      if (productVariation) {
+        _router.push("/dat-hang?type=NOW&id=" + productVariation.id);
+        console.log("buy now variation: ", productVariation);
+      }
+    } else {
+      console.log("add cart - size");
+      // for size
+      if (!selectedVariation2.value) {
+        notification.warning({
+          message: "Vui lòng chọn phân loại sản phẩm!",
+        });
+        return;
+      }
+      const productVariation = productDetail.value.bienTheDTOS.find(
+        (item) => item.giatri2 == Number(selectedVariation2.value)
+      );
+      console.log("variation: ", productVariation);
+      if (productVariation) {
+        _router.push("/dat-hang?type=NOW&id=" + productVariation.id);
+        console.log("buy now variation: ", productVariation);
+      }
+    }
+  }
+};
+
+const onClickImageBg = (src: string) => {
+  if (typeof src == "object") {
+    console.log("click image: ", src[0]);
+    activeImage.value = src[0];
+  } else activeImage.value = src;
+};
 const relatedProducts = ref([]);
 onMounted(() => {
-  ProductService
-    .chiTietSp(Number(_route.params.id))
+  ProductService.chiTietSp(Number(_route.params.id))
     .then((res) => {
       console.log("result:  ", res);
 
@@ -427,13 +587,16 @@ onMounted(() => {
       activeImage.value = res.anhChinh.url;
       productImages.value = pImgs;
 
-      ProductService.locSp({
-        dmGiay: res?.dmGiay?.id
-      }, 0, 4)
-        .then(res => {
-          console.log("related products: ", res);
-          relatedProducts.value = res.content;
-        })
+      ProductService.locSp(
+        {
+          dmGiay: res?.dmGiay?.id,
+        },
+        0,
+        4
+      ).then((res) => {
+        console.log("related products: ", res);
+        relatedProducts.value = res.content;
+      });
     })
     .catch((err) => {
       console.log("fetch product error", err);
